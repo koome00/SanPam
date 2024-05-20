3962447cb5aaffb8f80809ab1ba9d54ea33871ec
koome <koomemc@gmail.com>
2024-05-20 16:40:05 +0300

    project initialization files

---
+++ server/main.py
@@ -0,0 +1,16 @@
+from flask import Flask, jsonify
+from flask_cors import CORS
+
+app = Flask(__name__)
+cors = CORS(app, origins='*')
+
+
+@app.route("/api/users", methods=["GET"])
+def users():
+    return jsonify({"users": [
+                    "john",
+                    "alice"
+    ]})
+
+if __name__ == "__main__":
+    app.run(debug=True, port=8080)
